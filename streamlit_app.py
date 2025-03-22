83b5635e77f4cfa57f6bf888a4173349bf8c9b5e
Shinnosuke Yanagawa <90597106+ShinnnosukeYanagawa@users.noreply.github.com>
2025-03-23 02:35:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 import torch
+import torch.nn as nn
+import torch.optim as optim
 
-x = torch.tensor([1,2,3,4,5])
-st.write(x)
+class Model(nn.Module):
+  def __init__(self):
+    super(Model,self).__init__()
+    self.w = nn.Parameter(torch.randn(1,dtype=torch.float32,requires_grad=True))
+    self.b = nn.Parameter(torch.randn(1,dtype=torch.float32,requires_grad=True))
+  def forward(self,x):
+    return self.w*x + self.b
+
+model = Model()
+
+x = torch.tensor(float(input("今日の気温を教えてください"))).unsqueeze(dim=2)
+y = model(x)
+
+st.write(f"今日の売り上げは{y}です")
